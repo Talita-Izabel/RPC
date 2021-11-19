e74d4f4911b9ff68088586b004235f09dd4c6d56
Talita-Izabel <talitazbl@gmail.com>
2021-11-18 22:13:05 -0300

    add threading

--- rpc.py
+++ rpc.py
@@ -1,11 +1,25 @@
+import queue
 import socket
+import threading, queue
 
-def connection(self, data):
+
+def connection(self, data, queue):
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     clientSocket.sendto(data.encode('utf-8'), (self.server, self.port))
     modifiedMessage, serverAdress = clientSocket.recvfrom(2048)
     clientSocket.close()
-    return modifiedMessage.decode('utf-8')
+    msg = modifiedMessage.decode('utf-8')
+    queue.put(msg)
+    return
+
+def startThreads(self, data):
+    q = queue.Queue()
+
+    thr = threading.Thread(target=connection, args=(self, data, q))
+    thr.start()
+    thr.join()
+    
+    return q.get()
 
 class OperacoesMatematicas:
     def __init__(self, server, port):
@@ -14,13 +28,16 @@ class OperacoesMatematicas:
 
     def soma(self, number1, number2):
         data = f'soma {number1} {number2}'
-        return connection(self, data)
+        # return connection(self, data)
+        return startThreads(self, data)
 
     def produto(self, number1, number2):
         data = f'produto {number1} {number2}'
-        return connection(self, data)
+        # return connection(self, data)
+        return startThreads(self, data)
 
     def fatorial(self, number):
         data = f'fatorial {number}'
-        return connection(self, data)
+        # return connection(self, data)
+        return startThreads(self, data)
 
